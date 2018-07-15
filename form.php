d329bb2dd81369416cfc0687e89e1ee8d0b3f70b
ArtemijFedorov <original_ti@mail.ru>
2018-07-15 22:31:16 +0300

    initial commit

---
+++ form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anonymous
+ * Date: 14.07.2018
+ * Time: 17:25
+ */
---
+++ get.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anonymous
+ * Date: 14.07.2018
+ * Time: 18:26
+ */
