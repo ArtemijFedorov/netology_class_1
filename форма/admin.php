def46edde6c9696aa836577b697e4cf764ec623b
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-07-23 02:59:13 +0300

    Update admin.php

--- форма/admin.php
+++ форма/admin.php
@@ -5,32 +5,14 @@
     <title>Форма для отправки</title>
 </head>
 <body>
-<?php
-//$url = ;
-//$test = file_get_contents($url);
-//var_dump($test);
-?>
-<form action="list.php" method="post" enctype="multipart/form-data">
+<form enctype="multipart/form-data" action="list.php" method="POST">
     <div>Ты начинаешь свой путь в бездну! Ты готов пройти тест по PHP?</div>
-    <div><input type="hidden" name="test_yes" value="<?= $test?>">Давай рискнём<Br></div>
-   <div><input type="radio" name="test_no">Нет, давай лучше в PUBG<Br></div>
+    <div><input type="file" name="test"></div>
 
     <input type="submit" value="Отправить">
 
 </form>
-<?php
-//$array = [
-//'compilerOptions'=>[
-//'module'=> "commonjs",
-//"target": "es5",
-//"sourceMap": true
-//],
-//"exclude": [
-//"node_modules"
-//];
-//echo $array;
-////"http://university.netology.ru/u/fedorov/FedorovAO/test.json"
-//?>
 </body>
+
 
 
