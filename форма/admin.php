fa336128c8856dcb3f13446cb184abba1cbc9fd1
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-07-27 00:48:07 +0300

    Update admin.php

--- форма/admin.php
+++ форма/admin.php
@@ -5,44 +5,10 @@
     <title>Форма для отправки</title>
 </head>
 <body>
-<form enctype="multipart/form-data" action="admin.php" method="POST">
+<form enctype="multipart/form-data" action="list.php" method="POST">
     <div>Ты начинаешь свой путь в бездну! Ты готов пройти тест по PHP?</div>
     <div><input type="file" name="test"></div>
 <!--    <img src="http://university.netology.ru/u/fedorov/FedorovAO/test.json">-->
-
-    <input type="submit" value="Отправить">
-
-</form>
-
-<?php
-if (!empty($_FILES) || array_key_exists('test', $_FILES)) {
-    move_uploaded_file($_FILES['test']['tmp_name'], 'test1.json');
-//    var_dump( '<img src= "test1.json"/>');
-    $download = file_get_contents(__DIR__ . '/test1.json');
-    echo '<pre>';
-    //var_dump($download);
-} else {
-    echo 'Проблема в том, что тест не загружен';
-}
-$qestions = json_decode($download,true);
-if (!empty(is_array($qestions))) {
-    echo 'Всё ок!';
-} else {
-    echo 'Ты что-то сделал не так';
-}
-//var_dump($qestions);
-?>
-<form action="test.php" method="POST">
-
-  <?php  foreach ($qestions as $qestion => $qest){ ?>
-    <fieldset>
-        <legend><?php echo $qest ['legend'];?></legend>
-        <label><input type="radio" name="<?php echo $qest ['qest5'];?>" value="<?php $qest ['qest6'];?>"> <?php echo $qest ['qest1'];?></label>
-        <label><input type="radio" name="<?php echo $qest ['qest5'];?>" value="<?php $qest ['qest6'];?>"> <?php echo $qest ['qest2'];?></label>
-        <label><input type="radio" name="<?php echo $qest ['qest5'];?>" value="<?php $qest ['qest6'];?>"> <?php echo $qest ['qest3'];?></label>
-        <label><input type="radio" name="<?php echo $qest ['qest5'];?>" value="<?php $qest ['qest6'];?>"> <?php echo $qest ['qest4'];?></label>
-    </fieldset>
-  <?php } ?>
     <input type="submit" value="Отправить">
 </form>
 </body>
