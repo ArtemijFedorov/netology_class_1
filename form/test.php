971f256bed979601886121d2597b913c04fed121
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-07-31 03:10:46 +0300

    Update test.php

--- form/test.php
+++ form/test.php
@@ -1,23 +1,14 @@
 <?php
 if (!empty($_POST)){
-    if (array_key_exists('age', $_POST)) {
-        $options = [
-            'options' => [
-                'min_range' => 18,
-                'max_range' => 150
-            ]
-        ];
-
-        $validate = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT, $options);
-
-        if ($validate) {
-            echo 'Возраст: ' .$validate;
-        } else {
-            echo 'Данные введены не верно';
+    $testQ = $_POST;
+    foreach ($testQ as $testA => $item){
+        echo '<pre>';
+//        var_dump($item);
+        if ($item == "true"){
+            echo 'Правильный ответ';
+        }else{
+            echo 'Не правильный ответ';
         }
+
     }
 }
-
-echo htmlspecialchars($_POST['name']);
-echo "<pre>";
-echo $_POST['surname'];
