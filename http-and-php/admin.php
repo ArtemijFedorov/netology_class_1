d53cd1e5800188bd4992c6d16a9b9091a9f01168
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-08-03 07:28:26 +0300

    Update and rename files_http.php to admin.php

---
+++ http-and-php/admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Форма для отправки</title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="admin.php" action ="list.php" method="POST">
+    <div>Ты начинаешь свой путь в бездну! <br>Ты готов пройти тест по PHP?</div>
+    <div><input type="file" name="test"></div>
+    <input type="submit" value="Отправить">
+</form>
+<?php
+if (!empty($_FILES) || array_key_exists('test', $_FILES)) {//Проверяет наличее ключа в суперглобальной переменной
+    move_uploaded_file($_FILES['test']['tmp_name'], 'PHPtest.json');//Загружает и переименовывает файл на сервер
+    echo "<pre>";
+    echo '<b>Файл загружен</b>';
+    header("Location: list.php");
+} else {
+    echo 'Файл не загружен';
+}
+?>
+<!--<p><a href="list.php">Перейти к тесту</a></p>-->
+</body>
+</html>
