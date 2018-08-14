5a3b10c2c29e34d257d524209f3ad3ab131082db
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-08-14 23:24:12 +0300

    Update admin.php

--- форма/admin.php
+++ форма/admin.php
@@ -5,10 +5,9 @@
     <title>Форма для отправки</title>
 </head>
 <body>
-<form enctype="multipart/form-data" action="admin.php" method="POST">
-    <div>Ты начинаешь свой путь в бездну! Ты готов пройти тест по PHP?</div>
+<form enctype="multipart/form-data" action="admin.php" action ="list.php" method="POST">
+    <div>Ты начинаешь свой путь в бездну! <br>Ты готов пройти тест по PHP?</div>
     <div><input type="file" name="test"></div>
-<!--    <img src="http://university.netology.ru/u/fedorov/FedorovAO/test.json">-->
     <input type="submit" value="Отправить">
 </form>
 <?php
@@ -16,10 +15,8 @@ if (!empty($_FILES) || array_key_exists('test', $_FILES)) {//Проверяет 
     move_uploaded_file($_FILES['test']['tmp_name'], 'PHPtest.json');//Загружает и переименовывает файл на сервер
     echo "<pre>";
     echo '<b>Файл загружен</b>';
+    header("Location: list.php");
 } else {
     echo 'Файл не загружен';
 }
 ?>
-</body>
-</html>
-
