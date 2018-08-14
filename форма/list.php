cef06096ef0de6f258407eb2e153980c7f2996ed
ArtemijFedorov <32965193+ArtemijFedorov@users.noreply.github.com>
2018-08-14 23:25:00 +0300

    Update list.php

--- форма/list.php
+++ форма/list.php
@@ -2,17 +2,37 @@
 <html lang="en-ru">
 <head>
     <meta charset="UTF-8">
-    <title>Форма для отправки</title>
+    <title>Тест по PHP</title>
 </head>
+
 <?php
-if (!empty($_FILES) || array_key_exists('test', $_FILES)) {//Проверяет наличее ключа в суперглобальной переменной
-    move_uploaded_file($_FILES['test']['tmp_name'], 'PHPtest.json');//Загружает и переименовывает файл на сервер
+
+$download = file_get_contents(__DIR__ . '/PHPtest.json');
+$qestions = json_decode($download,true);
+if (!empty(is_array($qestions))) {
+    echo "<a href='test.php'><i>Тест №1</i></a>";
+} else {
+    echo 'Ты что-то сделал не так';
+}
+//var_dump($qestions);
+?>
+<p>Загрузить ещё тест?</p>
+<form enctype="multipart/form-data" action ="list.php" method="POST">
+    <div><input type="file" name="test1"></div>
+    <input type="submit" value="Отправить">
+</form>
+<?php
+if (!empty($_POST) || array_key_exists('test', $_POST)) {//Проверяет наличее ключа в суперглобальной переменной
+    var_dump($_POST);
+    move_uploaded_file($_POST['test1']['tmp_name'], 'PHPtest1.json');//Загружает и переименовывает файл на сервер
     echo "<pre>";
     echo '<b>Файл загружен</b>';
+
+//    header("Location: list.php");
 } else {
     echo 'Файл не загружен';
 }
 ?>
-<body>
+
 </body>
 </html>
